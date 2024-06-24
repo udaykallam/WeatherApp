fd4337bd846c5c2b9dc367a3a279e4047434fe40
Uday Kallam <udayreddykallam@gmail.com>
2024-06-24 10:06:39 +0530

    Update settings.py

--- weatherapp/weatherapp/settings.py
+++ weatherapp/weatherapp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pz55uf_gg5-7cs6k$%yi!-+zjplt)$(=xda_t(r&l(a_=q#v+7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
